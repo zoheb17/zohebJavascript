896759bbbeb4324efca3735b8b1eec6645cac52c
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-23 11:42:56 +0000

    this is the example of  objec in Non primitive data type

---
+++ 20.js
@@ -0,0 +1,20 @@
+    // let number = ("235")
+
+    // console.log(number, h typeof number)
+    // let x 
+    // console.log(x ,typeof x)
+    // x = 8
+    // console.log(x ,typeof x)
+    // x = x +""
+    //  console.log(x ,typeof x)
+    //  x = x - 2
+    // console.log(x ,typeof x)
+
+let myInfo ={
+    firstName:"zoheb",
+    age:21,
+    contact:234567898,
+    adress:"ganga nagar" 
+
+}
+console.log(typeof myInfo)
