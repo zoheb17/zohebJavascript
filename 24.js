825a274095c1475ab721793df212e3813b5776b8
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-23 15:13:47 +0000

    if else statement

---
+++ 24.js
@@ -0,0 +1,71 @@
+// if else statement
+
+/* 
+
+if ... else is used to run code conditinoally - only 
+when a specific expression evaluate to true.
+
+THERE ARE THREE TYPE OF IF ELSE STATEMENT
+1.if statement
+2.if ...else statement
+3.if ...else if if ...else
+
+*/
+// let age =20;
+// if(age >= 24){
+//     console.log("hello adult")
+
+// }
+// console.log("hello child")
+
+// let age = 12;
+// if(age>=18){
+//     console.log("you can vote")
+// }else{
+//     console.log( "you cannot vote");
+// }
+
+
+
+
+
+// if ...else if ....else
+// if ===> 1st condition
+// else if ===> 2nd to nth condition
+// else ==> false condition
+
+
+// i want to make a program 
+// based on number
+
+let marks =74;
+if(marks >90){
+   console.log("A+") 
+}
+else if(marks >=80){
+    console.log("A")
+}
+else if(marks >=70){
+    console.log("B+")
+}
+else if(marks >=60){
+    console.log("B")
+
+} 
+else if (marks >=50){
+    console.log("c")
+}else{
+    console.log("you faild")
+}
+
+// 
+let temp =25;
+if(temp >=35){
+    console.log("it is hot")
+}
+else if(temp>=25){
+    console.log("it is normal")
+}
+else{
+    console.log("it is cold")
+}
