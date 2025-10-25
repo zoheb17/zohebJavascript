d04c5233826c45f93fb6ead87cd17602a3bcec37
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-25 07:25:34 +0000

    break & continue

---
+++ 29.js
@@ -0,0 +1,20 @@
+// break & continue
+
+// for (let i =1; i<=10; i++){
+//     console.log(i);
+//     if(i==5){
+
+   
+//     console.log("i have stoped");
+//     break;
+//      }
+// }
+
+for(let i =1; i <= 10; i++){
+    if(i==7){
+        console.log("my name is zoheb")
+        continue;
+        
+    }
+    console.log(i);
+}
