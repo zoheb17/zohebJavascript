bb0410bc010f263d8219b177a18690900fc3904c
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-25 07:16:09 +0000

    while loop syntax // runs while condition is true // 1.initialze // 2.condition // 3.code bloc // incriment number

---
+++ 26.js
@@ -0,0 +1,15 @@
+// while loop syntax
+// runs while condition is true
+// 1.initialze 
+// 2.condition
+// 3.code bloc
+// incriment number
+
+
+
+let i =1;
+while(i<11){
+    console.log(i)
+    i++;
+}
+
