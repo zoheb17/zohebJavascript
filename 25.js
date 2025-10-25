debf166d7e9203dd8530a08d562b352b2a88f77a
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-25 07:12:59 +0000

    loops in js

---
+++ 25.js
@@ -0,0 +1,28 @@
+// loops 
+/*
+loops are used to execute a block of code repeatedly
+as long a a condition is true.
+they help avoid writing the same code multilpe times 
+
+// there are two type of loop 
+1.basic loop ==> for  loop , while loop do while loop
+2.advance loop
+*/
+//  for loop:
+// runs a block of code a specific number of times.
+
+
+//  syntax for ..for loop
+// for(initialize,condition,increment)
+// {
+// code to repeat 
+// }
+// 1. initialize ==> where to start
+// 2.condition checks always for true to execute code block
+// 3.code block (what to repeat)
+// 4.increment the number
+
+for(let i =1; i <=5; i++)
+{
+    console.log("hello",i)
+}
