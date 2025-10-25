d3f1a40330de6882c790a7fa7a81f3bd37635bba
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-25 07:23:41 +0000

    table loop

---
+++ 28.js
@@ -0,0 +1,20 @@
+// table loop
+
+for( let i = 1; i<=100;i++) {
+    console.log(` 100 x ${i} = ${i *100}`)
+
+
+}
+
+let a =1 
+while( a<=100){
+    console.log(` 100 x ${a} = ${a *100}`)
+    a++
+}
+
+let c = 1;
+do{
+    console.log(` 100 x ${c} = ${c *100}`) 
+    c++;
+}while (c<=100)
+
