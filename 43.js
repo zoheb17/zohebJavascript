98ede5cd0b8e5d8bb1cd0c8b6e95985a57462b3a
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-31 15:06:11 +0000

    / for each method in  forEach() method

---
+++ 43.js
@@ -0,0 +1,28 @@
+// for each method in jS
+
+
+// 20. forEach() method
+
+
+// purpose : excutes a function for each elements 
+
+// return value : undifined
+
+// chainable :no 
+// changes orignasl Array :no
+
+// comman use :logging, saving data
+
+
+// ex
+
+
+let number = [1,2,3,4,5,6,]
+
+number.forEach((x)=>{
+    console.log(x * 2);
+})
+console.log(number); 
+
+
+
