86d82d14e33430c7d1f767a5e96789a59fb17111
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-05 14:45:57 +0000

    / ASychronous (non blocking)

---
+++ 56.js
@@ -0,0 +1,31 @@
+// ASychronous (non blocking)
+
+
+// these are delegated  to bowser or node.js api and run in the back ground , allowing js to move on
+
+
+
+// COMMON ASYNC OPERATION
+
+
+// type                                         example
+                                          
+// timers                                        setTimeout().setInterval
+
+// promises                                      promice.resolve(). promise.reject(), .then(), .catch
+
+// asy function                                  async function getData () { await fetch (....)}
+
+// fatch api                                    fetch("https://api.example.com")
+
+// network request                              XMLHttRequest, axios.get()
+
+// file system (node.js)                        fd.readFile(), fs writerFile() (async version)
+
+// database  q ueries                          mongoDB, MySQL queries etc                                
+
+// event listner                               button.addeventListener("click",...)
+
+// animation frames                            requestAnimationFrame()
+
+// web workers                                 backgroung thread for heavy computation
