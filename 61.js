82b4c04340ca4b7f547d9177edd4a7204643d108
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-07 11:34:35 +0000

    teneray operators

---
+++ 61.js
@@ -0,0 +1,13 @@
+// teneray operators 
+
+
+/*  its is a shoetcut for weiting if ...else statements - all in one 
+*/
+
+// syntax:
+// condition ? doThisIftrue :doThisefalse;
+
+
+let age = 23 
+let output=age>18? "vote": "no vote"
+console.log(output);
