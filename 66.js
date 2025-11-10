b9bf339599bca1441f90612caf5f92f422bf6ef2
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-10 15:36:47 +0000

    syntax difference

---
+++ 66.js
@@ -0,0 +1,26 @@
+// JSON vs Object 
+/*
+FEATURE                                JSON                              Object
+What it is           Text format for storing/transferring data         Structured storage system (usually in databases)
+Usage                   For APIs, configs, and communication           For querying and persistent data storage
+Structure                         key-value pairs                     Row & columns(SQL) or documents(NoSQL)
+Language support        Universal(Javascript, Python, etc)            Depends on DB engine(SQL, MongoDB,etc)
+Editable by humans?                     Yes                           Usually va queries or admin tables
+Support functions?                       No                           Some times(e.g., SQL functions, triggers)   
+
+*/ 
+ 
+
+// syntax difference
+
+/*
+FEATURE                                JSON                              Javascript Object
+Definition            Text format for storing & transfering data        In memory data structure used in code
+Quotes for keys            Mandatory double Quotes ("")                 Optional quotes- can use key, 'key' or "key"
+String values              Must use double quotes only                  Can use dingle or double quotes
+Functions allowed?                      No                              Yes
+Comments allowed?                       No                              Yes
+Data types allowed    string, number, boolean, null, object, aray       Everything JSON allows + functions, symbols, undefined, etc
+Usage                  For APIs,config files, and storage               For logic inside JS problems
+*/
+  
