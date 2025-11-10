2596aa0dbc3cf373f01520c1bcf388ffb3b4ac15
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-10 15:33:55 +0000

    what is json

---
+++ 65.js
@@ -0,0 +1,31 @@
+//  what is json 
+
+// json stand for javaScript object notation - it's a light weight format for storing and transferring data betwwen a client jn a server 
+
+// JSON (javaScript object notation) is the univevarsal languageg of data interchange in modern computing its lightweight human readable and machibne- parasable .Every major programming language (python java fo rust etc ) car parse generate json easil
+// 
+// universal format (used in 99% api
+// no need for extra encoding/decoding layers 
+
+// it looks like javascript object but it acullay a text format 
+
+// property                    
+// language independent   ------------------------    works in all languages (js python java go EventCounts.)        
+
+// lightweight            --------------------------  faster than xml or yaml
+
+// human + machine readable                            easy for both human and program
+
+// flexible                                              supports nested structures 
+
+// standardized                                        define by ecma - 404
+
+// secure                                             no code execution(unlike xml parsers)
+
+// extensible
+
+
+
+
+// 
+
